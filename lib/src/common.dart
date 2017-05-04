0b6b00873e618def9ec70e8e54e85eaaec6a16b6
Luke <freeman.luke@gmail.com>
2017-05-04 12:33:40 -0700

    add header comments

--- lib/src/common.dart
+++ lib/src/common.dart
@@ -1,3 +1,10 @@
+/*
+ * router
+ * A Posse Production
+ * http://goposse.com
+ * Copyright (c) 2017 Posse Productions LLC. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
 part of router;
 
 ///
