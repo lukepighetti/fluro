a48b7224689e6cf19ac31502d676f34b4e0da892
Luke <freeman.luke@gmail.com>
2018-04-05 14:09:47 -0700

    fix warnings

--- example/lib/config/route_handlers.dart
+++ example/lib/config/route_handlers.dart
@@ -33,28 +33,30 @@ var demoFunctionHandler = new Handler(
       String message = params["message"]?.first;
       showDialog(
         context: context,
-        child: new AlertDialog(
-          title: new Text(
-            "Hey Hey!",
-            style: new TextStyle(
-              color: const Color(0xFF00D6F7),
-              fontFamily: "Lazer84",
-              fontSize: 22.0,
-            ),
-          ),
-          content: new Text("$message"),
-          actions: <Widget>[
-            new Padding(
-              padding: new EdgeInsets.only(bottom: 8.0, right: 8.0),
-              child: new FlatButton(
-                onPressed: () {
-                  Navigator.of(context).pop(true);
-                },
-                child: new Text("OK"),
+        builder: (context) {
+          return new AlertDialog(
+            title: new Text(
+              "Hey Hey!",
+              style: new TextStyle(
+                color: const Color(0xFF00D6F7),
+                fontFamily: "Lazer84",
+                fontSize: 22.0,
               ),
             ),
-          ],
-        ),
+            content: new Text("$message"),
+            actions: <Widget>[
+              new Padding(
+                padding: new EdgeInsets.only(bottom: 8.0, right: 8.0),
+                child: new FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(true);
+                  },
+                  child: new Text("OK"),
+                ),
+              ),
+            ],
+          );
+        },
       );
     });
 
@@ -70,5 +72,5 @@ var deepLinkHandler = new Handler(handlerFunc: (BuildContext context, Map<String
   if (colorHex != null && colorHex.length > 0) {
     color = new Color(ColorHelpers.fromHexString(colorHex));
   }
-  return new DemoSimpleComponent(message: "DEEEEEP LINK!!!", color: color, result: result);  
+  return new DemoSimpleComponent(message: message, color: color, result: result);
 });
