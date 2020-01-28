588903bc33593e2cc110b3f5382d0e77407626f7
Dennis Kugelmann <kugelmann.dennis@gmail.com>
2020-01-28 15:01:22 +0100

    Add flutter/foundation import

--- lib/src/router.dart
+++ lib/src/router.dart
@@ -12,6 +12,7 @@ import 'dart:io';
 
 import 'package:fluro/fluro.dart';
 import 'package:fluro/src/common.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
