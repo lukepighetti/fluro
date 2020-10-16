c84ed03c5f49705d8d716763dd578c0e5a9e27df
lukepighetti <lukepighetti@gmail.com>
2020-10-16 19:03:20 -0400

    add BuildContext.settings and BuildContext.arguments

--- lib/fluro.dart
+++ lib/fluro.dart
@@ -9,5 +9,6 @@
 library fluro;
 
 export 'src/common.dart';
+export 'src/extensions.dart';
 export 'src/fluro_router.dart';
 export 'src/tree.dart';
---
+++ lib/src/extensions.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+extension FluroBuildContextX on BuildContext {
+  /// Convenience method to retreive [RouteSettings] via
+  /// `ModalRoute.of(context).settings`
+  RouteSettings get settings => ModalRoute.of(this).settings;
+
+  /// Helper to get [RouteSettings.arguments] via
+  /// `ModalRoute.of(context).settings.arguments`
+  Object get arguments => ModalRoute.of(this).settings.arguments;
+}
