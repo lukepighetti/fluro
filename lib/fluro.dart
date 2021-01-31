c9843b4c0f9ce26515457347a199c325629d4eec
lukepighetti <lukepighetti@gmail.com>
2021-01-31 11:38:05 -0500

    update license year

--- lib/fluro.dart
+++ lib/fluro.dart
@@ -3,7 +3,7 @@
  * Created by Yakka
  * https://theyakka.com
  * 
- * Copyright (c) 2020 Luke Pighetti. All rights reserved.
+ * Copyright (c) 2021 Luke Pighetti. All rights reserved.
  * See LICENSE for distribution and usage details.
  */
 library fluro;
